bbe7d66290b0fc18cc36e1fad0ef72a973f6804a
YuriPetukhov <petuchowjuri@yahoo.de>
2023-04-10 14:21:48 +0300

    Второй вариант решения для 3 задачи.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3_2();
         task3();
         task4();
 
@@ -50,6 +51,40 @@ public class Main {
         System.out.println();
     }
 
+    public static void task3_2 () {
+        System.out.println("Задача 3 - второй вариант");
+
+        int [] array1 = new int [12];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        float [] array2 = {1.57f, 7.654f, 9.986f};
+
+        String [] array3 = new String []{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+        for (int i = array1.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(array1[i]);
+                break;
+            }
+            System.out.print(array1[i] + ", ");
+        }
+        for (int i = array2.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(array2[i]);
+                break;
+            }
+            System.out.print(array2[i] + ", ");
+        }
+            for (int i = array3.length - 1; i >= 0; i--) {
+                if (i == 0) {
+                    System.out.println(array3[i]);
+                    break;
+                }
+                System.out.print(array3[i] + ", ");
+            }
+    }
     public static void task3 () {
         System.out.println("Задача 3");
 
@@ -89,9 +124,6 @@ public class Main {
             }
         }
         System.out.println(Arrays.toString(array1));
-
-
-
     }
 
 }
